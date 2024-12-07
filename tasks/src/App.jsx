fe4f4232eeaeebc2261bde33581cdb9a43c0c937
jasika03 <jasika1452@gmail.com>
2024-12-07 12:21:10 +0530

    usestate

--- tasks/src/App.jsx
+++ tasks/src/App.jsx
@@ -1,17 +1,12 @@
-import React from 'react';
-import Intro from './components/app'; 
-import {Accordion} from './components/app';
-import Jasika from './assets/jasika.jpg';
+import React from 'react'
+import Counter from './components/app'
 
-function App() {
+const App = () => {
   return (
     <div>
-      <Intro img={Jasika}/>
-      <Accordion title1="1st" content1="This is the first item's accordion body. It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. Its also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow." 
-      title2="2nd" content2="This is the second item's accordion body. It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow."
-      title3="3rd" content3="This is the third item's accordion body. It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow."/>
+      <Counter/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- tasks/src/components/app.jsx
+++ tasks/src/components/app.jsx
@@ -1,79 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react'
 
-function Intro(props) {
-  return(
-  <>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">{props.name}</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-      </form>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-      </ul>
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </div>
+function Counter(){
+  const[count, setCount] = useState(0);
+  const increment = () =>{
+    setCount(count+1);
+  }
+  const decrement= () => {
+    setCount(count - 1);
+  }
+  return (
+    <div style={{margin:'35vh'}}>
+      <h1 style= {{textAlign: 'center'}}>COUNT: {count}</h1>
+      <div class="d-grid gap-2 col-6 mx-auto">
+    <button onClick={increment} type="button" class="btn btn-outline-primary">INCREMENT</button>
+    <button onClick={decrement} type="button" class="btn btn-outline-primary">DECREMENT</button>
   </div>
-</nav>
-  </>);
-}
-
-Intro.defaultProps= {name: "name here"}
-export default Intro;
-
-function Accordion(props) {
-  return(
-    <>
-    <div class="accordion" id="accordionExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        {props.title1}
-      </button>
-    </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        {props.content1}
-      </div>
+      
     </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-      {props.title2}
-      </button>
-    </h2>
-    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        {props.content2}
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-      {props.title3}
-      </button>
-    </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        {props.content3}
-      </div>
-    </div>
-  </div>
-</div>
-    </>
+    
   );
 }
+export default Counter
 
-export {Accordion};
